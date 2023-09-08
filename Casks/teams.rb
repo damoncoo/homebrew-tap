762f72984bb1c9f17c2393396ed9a9f772a083da
Damon Cheng <4880373+damoncoo@users.noreply.github.com>
2023-09-09 00:31:50 +0800

    Create teams.rb

---
+++ Casks/teams.rb
@@ -0,0 +1,15 @@
+cask "teams" do
+  arch arm: "darwin-arm64"
+
+  version "1.0"
+  sha256 "26e5ec8fdf86d4a1bd9f5e8af04220b573cd29fd5ff8a19161a3d3c49032ae5b"
+
+  url "https://damoncoo.s3.ap-southeast-1.amazonaws.com/Teams.tar.gz"
+  name "Microsoft Teams"
+  name "Teams"
+  desc "Teams app from Microsoft"
+  homepage "https://www.microsoft.com/en-us/microsoft-teams/log-in"
+
+  app "Microsoft Teams.app"
+  
+end
